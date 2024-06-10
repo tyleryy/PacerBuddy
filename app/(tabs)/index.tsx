1ce238f5ec5af7b4dd8dd667e9d1cd9cc28552d3
Tyler Yu <tyleryy@uci.edu>
2024-06-09 22:20:45 -0700

    feat: parseFloat

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,86 +10,86 @@ import { View } from "react-native";
 import Toast from "react-native-toast-message";
 
 export default function HomeScreen() {
-    const [start, setStart] = useState(true);
-    const [pace, setPace] = useState("");
+  const [start, setStart] = useState(true);
+  const [pace, setPace] = useState("");
 
-    const showToastError = () => {
-        Toast.show({
-            type: "error",
-            text1: "Rover Stopped 🛑",
-        });
-    };
+  const showToastError = () => {
+    Toast.show({
+      type: "error",
+      text1: "Rover Stopped 🛑",
+    });
+  };
 
-    const showToastSuccess = () => {
-        Toast.show({
-            type: "success",
-            text1: "Rover Started 🚀",
-        });
-    };
+  const showToastSuccess = () => {
+    Toast.show({
+      type: "success",
+      text1: "Rover Started 🚀",
+    });
+  };
 
-    useEffect(() => {}, [start]);
+  useEffect(() => {}, [start]);
 
-    async function StartRover() {
-        await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/startrover`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ pace: parseInt(pace) }),
-        });
-        showToastSuccess();
-    }
+  async function StartRover() {
+    await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/startrover`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pace: parseFloat(pace) }),
+    });
+    showToastSuccess();
+  }
 
-    async function KillRover() {
-        console.log(process.env.EXPO_PUBLIC_API_BASE_URL);
-        await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/killrover`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ pace: parseInt(pace) }),
-        });
-        showToastError();
-    }
+  async function KillRover() {
+    console.log(process.env.EXPO_PUBLIC_API_BASE_URL);
+    await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/killrover`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pace: parseFloat(pace) }),
+    });
+    showToastError();
+  }
 
-    return (
-        <SafeAreaView className="h-full">
-            <ScrollView
-                contentContainerStyle={{
-                    height: "100%",
+  return (
+    <SafeAreaView className="h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="h-screen w-screen flex flex-col px-5 items-center justify-center gap-5">
+          <View className="min-w-3/4">
+            <SpeedInput value={pace} setValue={setPace} />
+          </View>
+          <View className="min-w-1/2">
+            {!start ? (
+              <Button
+                variant="outline"
+                className="bg-red-500 hover:bg-red-600"
+                onPress={() => {
+                  setStart(true);
+                  KillRover();
                 }}
-            >
-                <View className="h-screen w-screen flex flex-col px-5 items-center justify-center gap-5">
-                    <View className="min-w-3/4">
-                        <SpeedInput value={pace} setValue={setPace} />
-                    </View>
-                    <View className="min-w-1/2">
-                        {!start ? (
-                            <Button
-                                variant="outline"
-                                className="bg-red-500 hover:bg-red-600"
-                                onPress={() => {
-                                    setStart(true);
-                                    KillRover();
-                                }}
-                            >
-                                <Text>Stop</Text>
-                            </Button>
-                        ) : (
-                            <Button
-                                variant="outline"
-                                className="bg-green-500"
-                                onPress={() => {
-                                    setStart(false);
-                                    StartRover();
-                                }}
-                            >
-                                <Text>Start</Text>
-                            </Button>
-                        )}
-                    </View>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+              >
+                <Text>Stop</Text>
+              </Button>
+            ) : (
+              <Button
+                variant="outline"
+                className="bg-green-500"
+                onPress={() => {
+                  setStart(false);
+                  StartRover();
+                }}
+              >
+                <Text>Start</Text>
+              </Button>
+            )}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
